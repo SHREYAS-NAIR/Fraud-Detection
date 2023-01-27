a533cda63e834232dc0a7de61ef93215b1c010fa
SHREYASNAIR129 <nairsm_1@rknec.edu>
2023-01-27 13:21:10 +0530

    Files before pipeline creation

---
+++ dataPreprocessing.py
@@ -0,0 +1,32 @@
+from dataLoading import load_data
+import pandas as pd
+from imblearn.over_sampling import RandomOverSampler
+
+def data_preprocessing():
+    #Loading data
+    data = pd.DataFrame(load_data())
+
+    #To visualize outliers
+    #seaborn.pairplot(data = data)
+    #plt.show()
+
+    #Changing values from scientific notation to much readable notation
+    pd.set_option('display.float_format', '{:.2f}'.format)
+
+    #Removing outliers and unwanted columns
+    data = data.drop(['Unnamed: 0'], axis=1)
+    data = data.drop(['TX_DATETIME'], axis=1)
+    data = data[(data["TX_AMOUNT"]<75000)]
+
+    #Checking imbalance in data
+    tx_fraud = data['TX_FRAUD']
+    print("Imbalanced data: \n",tx_fraud.value_counts())
+
+    #Random Oversampling of majority class to balance data
+    rus = RandomOverSampler(sampling_strategy="minority")
+    new_balanced_data, new_tx_fraud = rus.fit_resample(data,tx_fraud)
+
+    #Check the new balanced data
+    print("Balanced data: \n",new_tx_fraud.value_counts())
+
+    return(new_balanced_data)
---
+++ modelSelection.py
@@ -0,0 +1,73 @@
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression, SGDRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.svm import SVR
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
+import warnings
+warnings.filterwarnings('ignore')
+
+from sklearn.linear_model import LogisticRegression
+from dataPreprocessing import data_preprocessing
+
+# Mentioning the models for Classification
+dtc = tree.DecisionTreeClassifier()
+rfc = RandomForestClassifier()
+gbc = GradientBoostingClassifier()
+lr = LogisticRegression()
+knc = KNeighborsClassifier()
+svc = LinearSVC()
+
+def modelSelection():
+    data = data_preprocessing()
+    print(type(data))
+    # Assigning the featurs as X and trarget as y
+    X= data.drop(["TX_FRAUD"],axis =1)
+    y= data["TX_FRAUD"]
+    x_train, x_test, y_train, y_test = train_test_split(X, y,test_size=0.25, random_state=7)
+    algo_names = ["Decision Tree","Logistic Regression","Random Forest Classification","Gradient Boosting Classification","Linear Support Vector Classification","K Neighbours Classification"]
+    algo = [dtc, lr, rfc, gbc, svc, knc] 
+    
+    #Finding AI Scores for each classifier
+    Al_Scores = []
+    j=0
+    for i in algo:
+        train = i.fit(x_train,y_train)
+        score = train.score(x_test,y_test)
+        Al_Scores.append(score)
+        # print(score)
+        print(algo_names[j],":",score)
+        j+=1
+    
+    #Deciding final model
+    max_value= max(Al_Scores)
+    for i in range(len(Al_Scores)):
+        if Al_Scores[i] == max_value:
+            best_model = algo[i]
+            best_model_name = algo_names[i]
+    print("Best model is", best_model_name)
+
+    #Finding metrics for the best model and printing them
+    y_predict = best_model.predict(x_test)
+    precision = precision_score(y_test, y_predict, pos_label='positive', average='micro')
+    recall = recall_score(y_test, y_predict, pos_label='positive', average='micro')
+    accuracy = accuracy_score(y_test, y_predict)
+    f1score = f1_score(y_test, y_predict, pos_label='positive', average='micro')
+
+    print("Precision = ", precision)
+    print("f1score = ", f1score)
+    print("recall = ", recall)
+    print("accuracy = ", accuracy)
+    
+modelSelection()
