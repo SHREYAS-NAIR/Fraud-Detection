9c9673e8bce81083449371fd2dbfacd1d3003d3e
SHREYASNAIR129 <nairsm_1@rknec.edu>
2023-02-01 12:16:14 +0530

    changed file loading site

--- dataLoading.py
+++ dataLoading.py
@@ -4,5 +4,5 @@ os.system("pip3 install -r requirements.txt")
 import pandas as pd
 
 def load_data():
-    data = pd.read_csv("./Final_Transactions.csv")
+    data = pd.read_csv("https://media.githubusercontent.com/media/SHREYASNAIR129/Fraud-Detection/master/Final_Transactions.csv")
     return data
