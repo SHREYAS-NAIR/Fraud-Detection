ca20ffe38684ef440e3b5283e80395f0000d7d32
SHREYASNAIR129 <nairsm_1@rknec.edu>
2023-02-01 12:21:35 +0530

    checking dataLoading

--- dataLoading.py
+++ dataLoading.py
@@ -7,3 +7,4 @@ def load_data():
     data = pd.read_csv("https://media.githubusercontent.com/media/SHREYASNAIR129/Fraud-Detection/master/Final_Transactions.csv")
     print(data.head())
     return data
+load_data()
