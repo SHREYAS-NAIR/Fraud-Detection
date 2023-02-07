985c7c0b6d2a1577d0c7fd69743c8a1ee0f78570
ShreyasNair <shreyas.jarvis129@gamil.com>
2023-02-07 17:05:27 +0530

    removed sudo word

--- installingRequirements.py
+++ installingRequirements.py
@@ -1,6 +1,6 @@
 import os
 def installing_requirements():
     print("Installing requirements.")
-    os.system("sudo apt-get install python3-pip")
+    os.system("apt-get install python3-pip")
     os.system("pip3 install -r requirements.txt")
 installing_requirements()
